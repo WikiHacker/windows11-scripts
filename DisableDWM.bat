7ebe7791c4fc266fdb9e241e0a5ac479088a6665
shoober420 <shoober420@gmail.com>
2025-09-27 19:06:35 -0400

    Update DisableDWM.bat

--- DisableDWM.bat
+++ DisableDWM.bat
@@ -8,7 +8,7 @@ rem # https://youtu.be/K_4i6X5OAw8
 rem # https://youtu.be/1iXfUThaQ1Q
 rem # https://youtu.be/j6_GXujgM3Y
 
-rem # SystemApps REQUIRES SAFE MODE
+rem # SystemApps REQUIRES WINRE ENVIRONMENT
 
 rem # Backup dwm.exe (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "C:\Windows\System32\dwm.exe"
