70f72a422c2e6db16416dc250e1f203855d46766
shoober420 <shoober420@gmail.com>
2025-09-28 15:09:40 -0400

    Update WindowsUI_Login.bat

--- WindowsUI_Login.bat
+++ WindowsUI_Login.bat
@@ -6,3 +6,13 @@ icacls "C:\Windows\System32\Windows.UI.logon.dll" /grant:r %username%:F
 rem taskkill /im Windows.UI.logon.dll /f
 rem del "C:\Windows\System32\Windows.UI.logon.dll" /s /f /q
 ren "C:\Windows\System32\Windows.UI.logon.dll" "Windows.UI.logon.dll.bak"
+
+
+
+
+rem # Fix Black screen at login
+takeown /s %computername% /u %username% /f "C:\Windows\System32\Windows.UI.logon.dll"
+icacls "C:\Windows\System32\Windows.UI.logon.dll" /grant:r %username%:F
+rem taskkill /im Windows.UI.logon.dll /f
+rem del "C:\Windows\System32\Windows.UI.logon.dll" /s /f /q
+ren "C:\Windows\System32\Windows.UI.logon.dll.bak" "Windows.UI.logon.dll"
