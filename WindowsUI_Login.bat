15539a33ca2f483d0bcbac545dc8a71958b6d79e
shoober420 <shoober420@gmail.com>
2025-09-28 15:28:08 -0400

    Update WindowsUI_Login.bat

--- WindowsUI_Login.bat
+++ WindowsUI_Login.bat
@@ -1,5 +1,37 @@
 rem # Windows.UI.login.dll
 
+
+
+@echo off
+
+
+
+echo.
+echo Windows.UI.login.dll
+echo.
+echo 1. Terminal Login
+echo 2. Default Login
+echo 3. SKIP
+echo.
+choice /c 123C /m "Choose an option :"
+
+if 4 EQU %ERRORLEVEL% (
+   echo User chose to cancel.
+) else if 3 EQU %ERRORLEVEL% (
+   call :zcepe
+) else if 2 EQU %ERRORLEVEL% (
+   call :termoff
+) else if 1 EQU %ERRORLEVEL% (
+   call :termon
+) else if 0 EQU %ERRORLEVEL% (
+   echo User bailed out.
+)
+
+goto :eof
+
+:termon
+echo User chose Terminal Login ON
+
 rem # Fix Black Screen at Login
 takeown /s %computername% /u %username% /f "C:\Windows\System32\Windows.UI.logon.dll"
 icacls "C:\Windows\System32\Windows.UI.logon.dll" /grant:r %username%:F
@@ -7,8 +39,10 @@ rem taskkill /im Windows.UI.logon.dll /f
 rem del "C:\Windows\System32\Windows.UI.logon.dll" /s /f /q
 ren "C:\Windows\System32\Windows.UI.logon.dll" "Windows.UI.logon.dll.bak"
 
+goto :end
 
-
+:termoff
+echo User chose Default Login
 
 rem # Fix Black screen at login
 takeown /s %computername% /u %username% /f "C:\Windows\System32\Windows.UI.logon.dll"
@@ -16,3 +50,14 @@ icacls "C:\Windows\System32\Windows.UI.logon.dll" /grant:r %username%:F
 rem taskkill /im Windows.UI.logon.dll /f
 rem del "C:\Windows\System32\Windows.UI.logon.dll" /s /f /q
 ren "C:\Windows\System32\Windows.UI.logon.dll.bak" "Windows.UI.logon.dll"
+
+goto :end
+
+:zcepe
+echo User chose SKIP
+
+goto :end
+
+:end
+
+PAUSE
