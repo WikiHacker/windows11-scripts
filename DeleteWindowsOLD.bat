2fdf6e83309a3a4078b660ae5b688c893bf174b1
shoober420 <shoober420@gmail.com>
2025-09-30 10:29:18 -0400

    Update DeleteWindowsOLD.bat

--- DeleteWindowsOLD.bat
+++ DeleteWindowsOLD.bat
@@ -2,12 +2,11 @@ rem # Delete Windows.old directory
 
 cd "%USERPROFILE%\Downloads\SetACL (executable version)\64 bit"
 
-SetACL.exe -on "C:\Windows\SystemApps" -ot file -actn setowner -ownr n:"n:Administrators" -rec cont_obj
+SetACL.exe -on "C:\Windows.old" -ot file -actn setowner -ownr "n:Administrators" -rec cont_obj
 
-SetACL.exe -on "C:\Windows\SystemApps" -ot file -actn ace -ace "n:Administrators;p:full" -rec cont_obj
+SetACL.exe -on "C:\Windows.old" -ot file -actn ace -ace "n:Administrators;p:full" -rec cont_obj
 
 cd "%~dp0"
-
 
 
 
