14936d840820a94ec5e43e6cf1cc65137e9b99d2
shoober420 <shoober420@gmail.com>
2025-09-30 09:21:40 -0400

    Update DisableDWM.bat

--- DisableDWM.bat
+++ DisableDWM.bat
@@ -64,7 +64,7 @@ takeown /s %computername% /u %username% /f "C:\Windows\System32\dwminit.dll"
 icacls "C:\Windows\System32\dwminit.dll" /grant:r %username%:F
 rem taskkill /im DWMInit.dll /f
 rem del "C:\Windows\System32\dwminit.dll" /s /f /q
-ren "C:\Windows\System32\dwminit.dll" "DWMInit.dll.bak"
+ren "C:\Windows\System32\dwminit.dll" "dwminit.dll.bak"
 
 
 
