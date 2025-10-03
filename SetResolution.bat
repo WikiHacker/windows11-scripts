3c1e5778fc24a3a7edd9f9277f89fcee3319fba1
shoober420 <shoober420@gmail.com>
2025-10-03 19:51:20 -0400

    Create SetResolution.bat

---
+++ SetResolution.bat
@@ -0,0 +1,14 @@
+rem # SetResolution
+
+rem # -f sets refresh rate (-f 60)
+
+
+
+cd "%USERPROFILE%\Downloads"
+
+sr SET -w 640 -h 480 -b 32 -o 0
+
+
+cd "%USERPROFILE%\Downloads"
+
+sr SET -w 800 -h 600 -b 32 -o 0 
