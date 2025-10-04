88f3ddf08f75769ff1780f58969366f5ea24b71e
shoober420 <shoober420@gmail.com>
2025-10-04 00:20:11 -0400

    Update dwminit.bat

--- dwminit.bat
+++ dwminit.bat
@@ -7,6 +7,15 @@ takeown /s %computername% /u %username% /f "C:\Windows\System32\dwminit.dll"
 icacls "C:\Windows\System32\dwminit.dll" /grant:r %username%:F
 rem taskkill /im dwminit.dll /f
 rem del "C:\Windows\System32\dwminit.dll" /s /f /q
+ren "C:\Windows\System32\dwminit.dll.bak" "dwminit.dll"
+
+
+
+rem # Fix Mouse
+takeown /s %computername% /u %username% /f "C:\Windows\System32\dwminit.dll"
+icacls "C:\Windows\System32\dwminit.dll" /grant:r %username%:F
+rem taskkill /im dwminit.dll /f
+rem del "C:\Windows\System32\dwminit.dll" /s /f /q
 ren "C:\Windows\System32\dwminit.dll" "dwminit.dll.bak"
 
 PAUSE
